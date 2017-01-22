ac863ef62689e6330fac2f13e2ca33eed2032095
Gilberto Silva <gsilva@nearshoremx.com>
2017-01-22 12:37:44 -0600

    routes, and components

---
+++ app/routes/programmers.js
@@ -0,0 +1,7 @@
+import Ember from 'ember';
+
+export default Ember.Route.extend({
+  model() {
+    return ["Guido van Rossum", "Kjarne Stroustrup", "Ken Thompson", "Brian Kernighan", "Dennies Ritchie", "Linus Torvalds"]
+  }
+});
